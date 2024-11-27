2ddb7b55e1f82d454ff7d0a0698edac29b5c5eda
Sylvainoel <sylvain030502@gmail.com>
2024-11-27 15:04:29 +0100

    jupyter notebook

--- p3.py
+++ p3.py
@@ -3,4 +3,5 @@ from toy_script import load_data
 if __name__ == '__main__':
 
     #Data importation
-    X_train, y_train, X_test = load_data("C:/Users/sylva/Documents/universite/master_2/Intro_machine_learning/Projets")
+    X_train, y_train, X_test = load_data("./")
+    print(X_train[3500][0])
