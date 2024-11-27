96fd38e4a19c72e327c524b9a21468f7ea9ed829
Sylvainoel <sylvain030502@gmail.com>
2024-11-27 14:41:47 +0100

    Data

---
+++ p3.py
@@ -0,0 +1,6 @@
+from toy_script import load_data
+
+if __name__ == '__main__':
+
+    #Data importation
+    X_train, y_train, X_test = load_data("C:/Users/sylva/Documents/universite/master_2/Intro_machine_learning/Projets")
